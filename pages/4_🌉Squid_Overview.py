7c953ed18de4fb22810b8fc4539a8d3bbaec3a11
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-16 22:07:48 +0330

    Update 4_🌉Squid_Overview.py

--- pages/4_🌉Squid_Overview.py
+++ pages/4_🌉Squid_Overview.py
@@ -103,3 +103,148 @@ with col2:
     start_date = st.date_input("Start Date", value=pd.to_datetime("2023-12-01"))
 with col3:
     end_date = st.date_input("End Date", value=pd.to_datetime("2025-09-30"))
+
+# --- Row 1 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+@st.cache_data
+def load_kpi_data(start_date, end_date):
+    
+    start_str = start_date.strftime("%Y-%m-%d")
+    end_str = end_date.strftime("%Y-%m-%d")
+
+    query = f"""
+    WITH axelar_service AS (
+        -- Token Transfers
+        SELECT 
+            created_at, 
+            LOWER(data:send:original_source_chain) AS source_chain, 
+            LOWER(data:send:original_destination_chain) AS destination_chain,
+            recipient_address AS user, 
+            CASE 
+              WHEN IS_ARRAY(data:send:amount) THEN NULL
+              WHEN IS_OBJECT(data:send:amount) THEN NULL
+              WHEN TRY_TO_DOUBLE(data:send:amount::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:send:amount::STRING)
+              ELSE NULL
+            END AS amount,
+            CASE 
+              WHEN IS_ARRAY(data:send:amount) OR IS_ARRAY(data:link:price) THEN NULL
+              WHEN IS_OBJECT(data:send:amount) OR IS_OBJECT(data:link:price) THEN NULL
+              WHEN TRY_TO_DOUBLE(data:send:amount::STRING) IS NOT NULL AND TRY_TO_DOUBLE(data:link:price::STRING) IS NOT NULL 
+                THEN TRY_TO_DOUBLE(data:send:amount::STRING) * TRY_TO_DOUBLE(data:link:price::STRING)
+              ELSE NULL
+            END AS amount_usd,
+            CASE 
+              WHEN IS_ARRAY(data:send:fee_value) THEN NULL
+              WHEN IS_OBJECT(data:send:fee_value) THEN NULL
+              WHEN TRY_TO_DOUBLE(data:send:fee_value::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:send:fee_value::STRING)
+              ELSE NULL
+            END AS fee,
+            id, 
+            'Token Transfers' AS Service, 
+            data:link:asset::STRING AS raw_asset
+        FROM axelar.axelscan.fact_transfers
+        WHERE status = 'executed'
+          AND simplified_status = 'received'
+          AND (
+            sender_address ilike '%0xce16F69375520ab01377ce7B88f5BA8C48F8D666%' 
+            OR sender_address ilike '%0x492751eC3c57141deb205eC2da8bFcb410738630%'
+            OR sender_address ilike '%0xDC3D8e1Abe590BCa428a8a2FC4CfDbD1AcF57Bd9%'
+            OR sender_address ilike '%0xdf4fFDa22270c12d0b5b3788F1669D709476111E%'
+            OR sender_address ilike '%0xe6B3949F9bBF168f4E3EFc82bc8FD849868CC6d8%'
+          )
+
+        UNION ALL
+
+        -- GMP
+        SELECT  
+            created_at,
+            data:call.chain::STRING AS source_chain,
+            data:call.returnValues.destinationChain::STRING AS destination_chain,
+            data:call.transaction.from::STRING AS user,
+            CASE 
+              WHEN IS_ARRAY(data:amount) OR IS_OBJECT(data:amount) THEN NULL
+              WHEN TRY_TO_DOUBLE(data:amount::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:amount::STRING)
+              ELSE NULL
+            END AS amount,
+            CASE 
+              WHEN IS_ARRAY(data:value) OR IS_OBJECT(data:value) THEN NULL
+              WHEN TRY_TO_DOUBLE(data:value::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:value::STRING)
+              ELSE NULL
+            END AS amount_usd,
+            COALESCE(
+              CASE 
+                WHEN IS_ARRAY(data:gas:gas_used_amount) OR IS_OBJECT(data:gas:gas_used_amount) 
+                  OR IS_ARRAY(data:gas_price_rate:source_token.token_price.usd) OR IS_OBJECT(data:gas_price_rate:source_token.token_price.usd) 
+                THEN NULL
+                WHEN TRY_TO_DOUBLE(data:gas:gas_used_amount::STRING) IS NOT NULL 
+                  AND TRY_TO_DOUBLE(data:gas_price_rate:source_token.token_price.usd::STRING) IS NOT NULL 
+                THEN TRY_TO_DOUBLE(data:gas:gas_used_amount::STRING) * TRY_TO_DOUBLE(data:gas_price_rate:source_token.token_price.usd::STRING)
+                ELSE NULL
+              END,
+              CASE 
+                WHEN IS_ARRAY(data:fees:express_fee_usd) OR IS_OBJECT(data:fees:express_fee_usd) THEN NULL
+                WHEN TRY_TO_DOUBLE(data:fees:express_fee_usd::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:fees:express_fee_usd::STRING)
+                ELSE NULL
+              END
+            ) AS fee,
+            id, 
+            'GMP' AS Service, 
+            data:symbol::STRING AS raw_asset
+        FROM axelar.axelscan.fact_gmp 
+        WHERE status = 'executed'
+          AND simplified_status = 'received'
+          AND (
+            data:approved:returnValues:contractAddress ilike '%0xce16F69375520ab01377ce7B88f5BA8C48F8D666%' 
+            OR data:approved:returnValues:contractAddress ilike '%0x492751eC3c57141deb205eC2da8bFcb410738630%'
+            OR data:approved:returnValues:contractAddress ilike '%0xDC3D8e1Abe590BCa428a8a2FC4CfDbD1AcF57Bd9%'
+            OR data:approved:returnValues:contractAddress ilike '%0xdf4fFDa22270c12d0b5b3788F1669D709476111E%'
+            OR data:approved:returnValues:contractAddress ilike '%0xe6B3949F9bBF168f4E3EFc82bc8FD849868CC6d8%'
+          )
+    )
+    SELECT 
+        COUNT(DISTINCT id) AS "Total Number of Bridges", 
+        COUNT(DISTINCT user) AS "Total Numebr of Users", 
+        ROUND(SUM(amount_usd)) AS "Total Bridges Volume",
+        COUNT(distinct raw_asset) as "Number of Supported Tokens",
+        round(max(amount_usd)) as "Maximum Bridge Amount",
+        round(avg(amount_usd)) as "Average Bridge Amount",
+        round(median(amount_usd)) as "Median Bridge Amount",
+        count(distinct (source_chain || '➡' || destination_chain)) as "Number of Unique Routes"
+    FROM axelar_service
+    WHERE created_at::date >= '{start_str}' 
+      AND created_at::date <= '{end_str}'
+    """
+
+    df = pd.read_sql(query, conn)
+    return df
+
+# --- Load Data ----------------------------------------------------------------------------------------------------
+df_kpi = load_kpi_data(start_date, end_date)
+
+# --- KPI Row ------------------------------------------------------------------------------------------------------
+card_style = """
+    <div style="
+        background-color: #f9f9f9;
+        border: 1px solid #e0e0e0;
+        border-radius: 12px;
+        padding: 20px;
+        text-align: center;
+        box-shadow: 2px 2px 10px rgba(0,0,0,0.05);
+        ">
+        <h4 style="margin: 0; font-size: 20px; color: #555;">{label}</h4>
+        <p style="margin: 5px 0 0; font-size: 20px; font-weight: bold; color: #000;">{value}</p>
+    </div>
+"""
+
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    st.markdown(card_style.format(label="💸Volume of Bridging", value=f"${df_kpi["Total Bridges Volume"][0]:,}"), unsafe_allow_html=True)
+with col2:
+    st.markdown(card_style.format(label="🔄Number of Bridging Transactions", value=f"{df_kpi["Total Number of Bridges"][0]:,} Txns"), unsafe_allow_html=True)
+with col3:
+    st.markdown(card_style.format(label="👥Number of Unique Users", value=f"{df_kpi["Total Numebr of Users"][0]:,} Wallets"), unsafe_allow_html=True)
+with col4:
+    st.markdown(card_style.format(label="💎Number of Bridged Tokens", value=f"{df_kpi["Number of Supported Tokens"][0]:,}"), unsafe_allow_html=True)
+
+st.markdown("<br>", unsafe_allow_html=True)
+
+
