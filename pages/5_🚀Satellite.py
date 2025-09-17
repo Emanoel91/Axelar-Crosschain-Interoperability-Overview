511d10fc1ff108f9f34fcd852b92b8bd9b9e1891
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-17 14:54:57 +0330

    Update 5_🚀Satellite.py

--- pages/5_🚀Satellite.py
+++ pages/5_🚀Satellite.py
@@ -162,15 +162,15 @@ card_style = """
         text-align: center;
         box-shadow: 2px 2px 10px rgba(0,0,0,0.05);
         ">
-        <h4 style="margin: 0; font-size: 15px; color: #555;">{label}</h4>
+        <h4 style="margin: 0; font-size: 20px; color: #555;">{label}</h4>
         <p style="margin: 5px 0 0; font-size: 20px; font-weight: bold; color: #000;">{value}</p>
     </div>
 """
 
 col1, col2, col3 = st.columns(3)
 with col1:
-    st.markdown(card_style.format(label="Bridge Volume", value=f"${df_kpi_data["Volume of Transfers"][0]:,}"), unsafe_allow_html=True)
+    st.markdown(card_style.format(label="Bridge Volume", value=f"💸${df_kpi_data["Volume of Transfers"][0]:,}"), unsafe_allow_html=True)
 with col2:
-    st.markdown(card_style.format(label="Bridge Transactions", value=f"{df_kpi_data["Number of Transfers"][0]:,} Txns"), unsafe_allow_html=True)
+    st.markdown(card_style.format(label="Bridge Transactions", value=f"🚀{df_kpi_data["Number of Transfers"][0]:,} Txns"), unsafe_allow_html=True)
 with col3:
-    st.markdown(card_style.format(label="Unique Users", value=f"{df_kpi_data["Number of Users"][0]:,} Wallets"), unsafe_allow_html=True)
+    st.markdown(card_style.format(label="Unique Users", value=f"👥{df_kpi_data["Number of Users"][0]:,} Wallets"), unsafe_allow_html=True)
