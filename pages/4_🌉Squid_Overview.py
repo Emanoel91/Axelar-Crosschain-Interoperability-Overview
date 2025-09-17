ff4d848b19bad5906cbe710143f5a4f1c1c76287
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-17 05:13:42 +0330

    Update 4_🌉Squid_Overview.py

--- pages/4_🌉Squid_Overview.py
+++ pages/4_🌉Squid_Overview.py
@@ -886,3 +886,132 @@ fig2 = px.bar(top_user.sort_values("Number of Users", ascending=False), x="Path"
 fig2.update_traces(texttemplate='%{text}', textposition='inside')
 fig2.update_layout(xaxis={'categoryorder':'total descending'})
 st.plotly_chart(fig2, use_container_width=True)
+
+# --- Row 7 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+@st.cache_data
+def load_path_tracking(start_date, end_date):
+    start_str = start_date.strftime("%Y-%m-%d")
+    end_str = end_date.strftime("%Y-%m-%d")
+    
+    query = f"""
+    WITH axelar_service AS (
+      
+      SELECT 
+        created_at, 
+        LOWER(data:send:original_source_chain) AS source_chain, 
+        LOWER(data:send:original_destination_chain) AS destination_chain,
+        recipient_address AS user, 
+
+        CASE 
+          WHEN IS_ARRAY(data:send:amount) THEN NULL
+          WHEN IS_OBJECT(data:send:amount) THEN NULL
+          WHEN TRY_TO_DOUBLE(data:send:amount::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:send:amount::STRING)
+          ELSE NULL
+        END AS amount,
+
+        CASE 
+          WHEN IS_ARRAY(data:send:amount) OR IS_ARRAY(data:link:price) THEN NULL
+          WHEN IS_OBJECT(data:send:amount) OR IS_OBJECT(data:link:price) THEN NULL
+          WHEN TRY_TO_DOUBLE(data:send:amount::STRING) IS NOT NULL AND TRY_TO_DOUBLE(data:link:price::STRING) IS NOT NULL 
+            THEN TRY_TO_DOUBLE(data:send:amount::STRING) * TRY_TO_DOUBLE(data:link:price::STRING)
+          ELSE NULL
+        END AS amount_usd,
+
+        CASE 
+          WHEN IS_ARRAY(data:send:fee_value) THEN NULL
+          WHEN IS_OBJECT(data:send:fee_value) THEN NULL
+          WHEN TRY_TO_DOUBLE(data:send:fee_value::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:send:fee_value::STRING)
+          ELSE NULL
+        END AS fee,
+
+        id, 
+        'Token Transfers' AS "Service", 
+        data:link:asset::STRING AS raw_asset
+
+      FROM axelar.axelscan.fact_transfers
+      WHERE status = 'executed'
+        AND simplified_status = 'received'
+        AND (
+        sender_address ilike '%0xce16F69375520ab01377ce7B88f5BA8C48F8D666%' -- Squid
+        or sender_address ilike '%0x492751eC3c57141deb205eC2da8bFcb410738630%' -- Squid-blast
+        or sender_address ilike '%0xDC3D8e1Abe590BCa428a8a2FC4CfDbD1AcF57Bd9%' -- Squid-fraxtal
+        or sender_address ilike '%0xdf4fFDa22270c12d0b5b3788F1669D709476111E%' -- Squid coral
+        or sender_address ilike '%0xe6B3949F9bBF168f4E3EFc82bc8FD849868CC6d8%' -- Squid coral hub
+    ) 
+
+      UNION ALL
+
+      SELECT  
+        created_at,
+        lower(data:call.chain::STRING) AS source_chain,
+        lower(data:call.returnValues.destinationChain::STRING) AS destination_chain,
+        data:call.transaction.from::STRING AS user,
+
+        CASE 
+          WHEN IS_ARRAY(data:amount) OR IS_OBJECT(data:amount) THEN NULL
+          WHEN TRY_TO_DOUBLE(data:amount::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:amount::STRING)
+          ELSE NULL
+        END AS amount,
+
+        CASE 
+          WHEN IS_ARRAY(data:value) OR IS_OBJECT(data:value) THEN NULL
+          WHEN TRY_TO_DOUBLE(data:value::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:value::STRING)
+          ELSE NULL
+        END AS amount_usd,
+
+        COALESCE(
+          CASE 
+            WHEN IS_ARRAY(data:gas:gas_used_amount) OR IS_OBJECT(data:gas:gas_used_amount) 
+              OR IS_ARRAY(data:gas_price_rate:source_token.token_price.usd) OR IS_OBJECT(data:gas_price_rate:source_token.token_price.usd) 
+            THEN NULL
+            WHEN TRY_TO_DOUBLE(data:gas:gas_used_amount::STRING) IS NOT NULL 
+              AND TRY_TO_DOUBLE(data:gas_price_rate:source_token.token_price.usd::STRING) IS NOT NULL 
+            THEN TRY_TO_DOUBLE(data:gas:gas_used_amount::STRING) * TRY_TO_DOUBLE(data:gas_price_rate:source_token.token_price.usd::STRING)
+            ELSE NULL
+          END,
+          CASE 
+            WHEN IS_ARRAY(data:fees:express_fee_usd) OR IS_OBJECT(data:fees:express_fee_usd) THEN NULL
+            WHEN TRY_TO_DOUBLE(data:fees:express_fee_usd::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:fees:express_fee_usd::STRING)
+            ELSE NULL
+          END
+        ) AS fee,
+
+        id, 
+        'GMP' AS "Service", 
+        data:symbol::STRING AS raw_asset
+
+      FROM axelar.axelscan.fact_gmp 
+      WHERE status = 'executed'
+        AND simplified_status = 'received'
+        AND (
+            data:approved:returnValues:contractAddress ilike '%0xce16F69375520ab01377ce7B88f5BA8C48F8D666%' -- Squid
+            or data:approved:returnValues:contractAddress ilike '%0x492751eC3c57141deb205eC2da8bFcb410738630%' -- Squid-blast
+            or data:approved:returnValues:contractAddress ilike '%0xDC3D8e1Abe590BCa428a8a2FC4CfDbD1AcF57Bd9%' -- Squid-fraxtal
+            or data:approved:returnValues:contractAddress ilike '%0xdf4fFDa22270c12d0b5b3788F1669D709476111E%' -- Squid coral
+            or data:approved:returnValues:contractAddress ilike '%0xe6B3949F9bBF168f4E3EFc82bc8FD849868CC6d8%' -- Squid coral hub
+            ) 
+    )
+
+    SELECT 
+    source_chain || '➡' || destination_chain as "Route",
+    round(sum(amount_usd)) as "Volume",
+    round((avg(amount_usd)),1) as "Avg Volume per Txn",
+    count(distinct id) as "Bridges", 
+    count(distinct user) as "Bridgors",
+    round((sum(amount_usd)/count(distinct user)),1) as "Avg Volume per Bridgor",
+    round(count(distinct id)/count(distinct user)) as "Avg Bridge Count per User"
+    FROM axelar_service
+    WHERE created_at::date BETWEEN '{start_date}' AND '{end_date}'
+    GROUP BY 1
+    ORDER BY 4 DESC
+    """
+    return pd.read_sql(query, conn)
+
+df_path_tracking = load_path_tracking(start_date, end_date)
+ 
+# --- Display Table -------------------------------------------------------------------------------------------------
+st.subheader("🟡Squid Bridging Routes' Stats")
+df_display = df_path_tracking.copy()
+df_display.index = df_display.index + 1
+df_display = df_display.applymap(lambda x: f"{x:,}" if isinstance(x, (int, float)) else x)
+st.dataframe(df_display, use_container_width=True)
