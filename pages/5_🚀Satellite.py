b287f1bcd153a12f433222882e33d6117f4af664
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-17 15:12:39 +0330

    Update 5_🚀Satellite.py

--- pages/5_🚀Satellite.py
+++ pages/5_🚀Satellite.py
@@ -174,3 +174,68 @@ with col2:
     st.markdown(card_style.format(label="Bridge Transactions", value=f"{df_kpi_data["Number of Transfers"][0]:,} Txns"), unsafe_allow_html=True)
 with col3:
     st.markdown(card_style.format(label="Unique Users", value=f"{df_kpi_data["Number of Users"][0]:,} Wallets"), unsafe_allow_html=True)
+
+# --- Row 2 -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+@st.cache_data
+def get_ts_data(_conn, start_date, end_date, timeframe):
+    query = f"""
+    WITH overview AS (
+      WITH tab1 AS (
+        SELECT block_timestamp::date AS date, tx_hash, source_chain, destination_chain, sender, token_symbol
+        FROM AXELAR.DEFI.EZ_BRIDGE_SATELLITE
+        WHERE block_timestamp::date >= '{start_date}'
+      ),
+      tab2 AS (
+        SELECT 
+            created_at::date AS date, 
+            LOWER(data:send:original_source_chain) AS source_chain, 
+            LOWER(data:send:original_destination_chain) AS destination_chain,
+            sender_address AS user,
+            CASE WHEN TRY_TO_DOUBLE(data:send:amount::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:send:amount::STRING) END AS amount,
+            CASE 
+              WHEN TRY_TO_DOUBLE(data:send:amount::STRING) IS NOT NULL AND TRY_TO_DOUBLE(data:link:price::STRING) IS NOT NULL 
+              THEN TRY_TO_DOUBLE(data:send:amount::STRING) * TRY_TO_DOUBLE(data:link:price::STRING) END AS amount_usd,
+            SPLIT_PART(id, '_', 1) as tx_hash
+        FROM axelar.axelscan.fact_transfers
+        WHERE status = 'executed' 
+          AND simplified_status = 'received'
+          AND created_at::date >= '{start_date}'
+      )
+      SELECT tab1.date, tab1.tx_hash, tab1.source_chain, tab1.destination_chain, sender, token_symbol, amount, amount_usd
+      FROM tab1 
+      LEFT JOIN tab2 ON tab1.tx_hash=tab2.tx_hash
+    )
+    SELECT 
+      DATE_TRUNC('{timeframe}', date) AS date,
+      COUNT(DISTINCT tx_hash) AS transfers, 
+      COUNT(DISTINCT sender) AS users,
+      ROUND(SUM(amount_usd)) AS volume_usd,
+      ROUND(AVG(amount_usd)) AS avg_volume_tx
+    FROM overview
+    WHERE date >= '{start_date}' AND date <= '{end_date}'
+    GROUP BY 1
+    ORDER BY 1;
+    """
+    df = pd.read_sql(query, _conn)
+    return df
+# --- Load Time-Series Data from Snowflake -------------------
+ts_df = get_ts_data(conn, start_date, end_date, timeframe)
+
+# --- Display Charts (Row 3) ---------------------------------
+col1, col2 = st.columns(2)
+
+with col1:
+    fig1 = go.Figure()
+    fig1.add_bar(x=ts_df["DATE"], y=ts_df["TRANSFERS"], name="Bridge Txns", yaxis="y1")
+    fig1.add_trace(go.Scatter(x=ts_df["DATE"], y=ts_df["USERS"], name="Users", mode="lines", yaxis="y2"))
+    fig1.update_layout(title="Number of Users & Bridging Transactions Over Time", yaxis=dict(title="Txns count"), yaxis2=dict(title="Wallet count", overlaying="y", side="right"),
+        xaxis=dict(title=" "), barmode="group", legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5))
+    st.plotly_chart(fig1, use_container_width=True)
+
+with col2:
+    fig2 = go.Figure()
+    fig2.add_bar(x=ts_df["DATE"], y=ts_df["VOLUME_USD"], name="Bridge Volume", yaxis="y1")
+    fig2.add_trace(go.Scatter(x=ts_df["DATE"], y=ts_df["AVG_VOLUME_TX"], name="Avg Volume per Txn", mode="lines", yaxis="y2"))
+    fig2.update_layout(title="Volume of Transfers Over Time", yaxis=dict(title="$USD"), yaxis2=dict(title="$USD", overlaying="y", side="right"), xaxis=dict(title=" "), barmode="group",
+                      legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5))
+    st.plotly_chart(fig2, use_container_width=True)
