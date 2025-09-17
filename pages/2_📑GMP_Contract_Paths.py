07177f20c1352d2cc04b59d186259a341b8703b9
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-17 23:39:22 +0330

    Update 2_📑GMP_Contract_Paths.py

--- pages/2_📑GMP_Contract_Paths.py
+++ pages/2_📑GMP_Contract_Paths.py
@@ -130,3 +130,137 @@ with col2:
         title="Distribution of Contracts by Volume"
     )
     st.plotly_chart(fig_pie_volume, use_container_width=True)
+
+# --- Row 4 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# === Left function ==================================
+@st.cache_data
+def load_event_txn():
+
+    query = f"""
+    with tab1 as (
+select event, id, data:call.transaction.from::STRING as user, CASE 
+      WHEN IS_ARRAY(data:value) OR IS_OBJECT(data:value) THEN NULL
+      WHEN TRY_TO_DOUBLE(data:value::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:value::STRING)
+      ELSE NULL
+    END AS amount_usd
+from axelar.axelscan.fact_gmp)
+select event as "Event", count(distinct id) as "Txns count"
+from tab1
+group by 1
+order by 2 desc 
+
+    """
+
+    df = pd.read_sql(query, conn)
+    return df
+  
+# === right function ==============================
+@st.cache_data
+def load_event_route_data():
+
+    query = f"""
+    with tab1 as (
+select event, id, data:call.transaction.from::STRING as user, CASE 
+      WHEN IS_ARRAY(data:value) OR IS_OBJECT(data:value) THEN NULL
+      WHEN TRY_TO_DOUBLE(data:value::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:value::STRING)
+      ELSE NULL
+    END AS amount_usd,
+    LOWER(data:call.chain::STRING) AS source_chain,
+    LOWER(data:call.returnValues.destinationChain::STRING) AS destination_chain
+from axelar.axelscan.fact_gmp)
+
+select source_chain || '➡' || destination_chain as "Route", 
+count(distinct id) as "🔗Txns count", 
+count(distinct user) as "👥Users Count", 
+round(sum(amount_usd),1) as "💸Txns Value (USD)"
+from tab1
+where event in ('ContractCall','ContractCallWithToken')
+group by 1
+order by 2 desc 
+
+    """
+
+    df = pd.read_sql(query, conn)
+    return df
+
+# === Load Data ===================================================
+df_event_txn = load_event_txn()
+df_event_route_data = load_event_route_data()
+# === Tables =====================================================
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown("<h5 style='text-align:center; font-size:16px;'>Number of GMP Transactions By Events</h5>", unsafe_allow_html=True)
+    df_display = df_event_txn.copy()
+    df_display.index = df_display.index + 1
+    df_display = df_display.applymap(lambda x: f"{x:,}" if isinstance(x, (int, float)) else x)
+    styled_df = df_display.style.set_properties(**{"background-color": "#c9fed8"})
+    st.dataframe(styled_df, use_container_width=True, height=320)   
+
+with col2:
+    st.markdown("<h5 style='text-align:center; font-size:16px;'>Contract Calls Across Chains (Sorted by Txns Count)</h5>", unsafe_allow_html=True)
+    df_display = df_event_route_data.copy()
+    df_display.index = df_display.index + 1
+    df_display = df_display.applymap(lambda x: f"{x:,}" if isinstance(x, (int, float)) else x)
+    styled_df = df_display.style.set_properties(**{"background-color": "#c9fed8"})
+    st.dataframe(styled_df, use_container_width=True, height=320)
+
+# --- Row 5 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# === Left function ==================================
+@st.cache_data
+def load_active_contracts():
+
+    query = f"""
+    select date_trunc('month',created_at) as "Date", 
+    count(distinct call:returnValues:destinationContractAddress) as "Number of Destination Contracts"
+    from axelar.axelscan.fact_gmp
+    group by 1
+    order by 1
+
+    """
+
+    df = pd.read_sql(query, conn)
+    return df
+  
+# === right function ==============================
+@st.cache_data
+def load_new_contracts():
+
+    query = f"""
+    with tab1 as (
+select call:returnValues:destinationContractAddress as "Destination Contract", 
+min(created_at::date) as FIRST_TX
+from axelar.axelscan.fact_gmp
+group by 1)
+
+select date_trunc('month',first_tx) as "Date", count(distinct "Destination Contract") as "New Contracts", 
+sum("New Contracts") over (order by "Date" asc) as "Total New Contracts"
+from tab1
+group by 1
+order by 1
+
+    """
+
+    df = pd.read_sql(query, conn)
+    return df
+
+# === Load Data ===================================================
+df_active_contracts = load_active_contracts()
+df_new_contracts = load_new_contracts()
+# === Tables =====================================================
+col1, col2 = st.columns(2)
+
+with col1:
+    fig1 = go.Figure()
+    fig1.add_trace(go.Bar(x=df_new_contracts["Date"], y=df_new_contracts["New Contracts"], name="New Contracts", yaxis="y1", marker_color="#ff7f27"))
+    fig1.add_trace(go.Scatter(x=df_new_contracts["Date"], y=df_new_contracts["Total New Contracts"], name="Total New Contracts", mode="lines", yaxis="y2", 
+                              line=dict(color="#0ed145", width=2, dash="solid")))
+    fig1.update_layout(title="Number of New Destination Contracts Over Time", yaxis=dict(title="contract count"), yaxis2=dict(title="contract count", 
+                            overlaying="y", side="right"), barmode="group", legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5))
+    st.plotly_chart(fig1, use_container_width=True)
+
+with col2:
+    fig_contract = px.scatter(df_active_contracts, x="Date", y="Number of Destination Contracts", size="Number of Destination Contracts", color="Number of Destination Contracts", 
+                              color_continuous_scale="Viridis", title="Number of Active Destination Contracts Over Time", 
+                              labels={"Number of Destination Contracts": "number of contracts", "Date":""})
+    st.plotly_chart(fig_contract)
